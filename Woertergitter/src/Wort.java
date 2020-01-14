811f6fb6822b95d1a383de871a2b6510dbe84ad9
Prisca <p.rambach@icloud.com>
2020-01-14 22:37:34 +0100

    Wort: Methodenruempfe gefuellt

--- Woertergitter/src/Wort.java
+++ Woertergitter/src/Wort.java
@@ -1,27 +1,51 @@
 public class Wort {
-    private final String WORT; // jedes Wort sollten ausschließlich in Großbuchstaben gespeichert werden und sollte auch nicht veränderbar sein, dafür erstellt man dann neue Worte
+    private final String WORT; // jedes Wort sollten ausschließlich in Großbuchstaben gespeichert werden und sollte auch nicht veränderbar sein, dafuer erstellt man dann neue Worte
     private Richtung richtung;
     private int x;
     private int y;
 
     public Wort (String wort){ //Konstruktor: Wort ohne Loesung
-        WORT = wort;
+        this.WORT = wort;
+        this.richtung = null;
+        this.x = -1;
+        this.y = -1;
     }
 
-    public Wort (String wort, String richtung, int x, int y){ //Konstruktor: Wort mit Loesung
-        WORT = wort;
+    public Wort (String wort, Richtung richtung, int x, int y) throws IllegalArgumentException{ //Konstruktor: Wort mit Loesung
+        this.WORT = wort.toUpperCase().trim();
+        this.richtung = richtung;
+        if (x >= 0) {
+            this.x = x;
+        }else{
+            throw new IllegalArgumentException("x wert kleiner 0");
+        }
+        if (y >= 0) {
+            this.y = y;
+        }else{
+            throw new IllegalArgumentException("y wert kleiner 0");
+        }
     }
 
     public String getWord(){
-        return "";
+        return WORT;
     }
 
     public void getPositionInField(){ //Ausgabe in Konsole
-
+        System.out.println(" "); //TODO
     }
 
-    public void setSolution (String richtung, int x, int y){ //eventuell mit nested class
-
+    public void setSolution (Richtung richtung, int x, int y) throws IllegalArgumentException{ //eventuell mit nested class
+        this.richtung = richtung;
+        if (x >= 0) {
+            this.x = x;
+        }else{
+            throw new IllegalArgumentException("x wert kleiner 0");
+        }
+        if (y >= 0) {
+            this.y = y;
+        }else{
+            throw new IllegalArgumentException("y wert kleiner 0");
+        }
     }
 
 }
